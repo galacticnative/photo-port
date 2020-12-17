35b7408207f349dc95c8805f455d48799db62a6b
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-12-17 14:26:36 -0700

    update readme

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Modal from '../'
+// __tests__/Modal.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
 
+const mockToggleModal = jest.fn();
 const currentPhoto = {
-    name: 'Park bench',
-    category: 'landscape',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-    index: 1
+  name: 'Park bench',
+  category: 'landscape',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1
 };
 
 afterEach(cleanup);
 
 describe('Modal component', () => {
-    it('renders', () => {
-      // baseline render component test
-      
-    })
-      // snapshot test
+  it('renders', () => {
+    render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+  })
 
-      it('matches snapshot DOM node structure', () => {
-        // Arrange the snapshot - declare variables
-        // Assert the match
-      });
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
 describe('Click Event', () => {
-    it('calls onClose handler', () => {
-      // Arrange: Render Modal
-      // Act: Simulate click event
-      // Assert: Expected matcher
+  it('calls onClose handler', () => {
+    const { getByText } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+    fireEvent.click(getByText('Close this modal'))
 
-      const { getByText } = render(<Modal
-        onClose={mockToggleModal}
-        currentPhoto={currentPhoto}
-      />);
-
-      fireEvent.click(getByText('Close this modal'));
-
-      expect(mockToggleModal).toHaveBeenCalledTimes(1);
-    });
-}) 
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
+})  
